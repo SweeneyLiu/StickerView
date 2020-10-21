114362492609157e8421bde7c42a9b557256a69c
liushuwei <liushuwei@kuaishou.com>
2020-10-21 20:20:36 +0800

    Initial commit

---
+++ app/src/main/java/com/lsw/stickerview/HelloIconEvent.java
@@ -0,0 +1,26 @@
+package com.lsw.stickerview;
+
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.lsw.sticker.StickerIconEvent;
+import com.lsw.sticker.StickerView;
+
+/**
+ * @author wupanjie
+ * @see StickerIconEvent
+ */
+
+public class HelloIconEvent implements StickerIconEvent {
+  @Override public void onActionDown(StickerView stickerView, MotionEvent event) {
+
+  }
+
+  @Override public void onActionMove(StickerView stickerView, MotionEvent event) {
+
+  }
+
+  @Override public void onActionUp(StickerView stickerView, MotionEvent event) {
+    Toast.makeText(stickerView.getContext(), "Hello World!", Toast.LENGTH_SHORT).show();
+  }
+}
